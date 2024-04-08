2650b458174fea58faec3ae99381408abf27649f
CH Lin <97071307+hugo930713@users.noreply.github.com>
2024-04-09 07:04:40 +0800

    feat: 新增路徑及頁面元件
    將路徑設定新增至 App.js 檔案中，並引入相應的頁面元件：
    - 首頁: Home
    - 會員頁面: Member
    - 購物車頁面: Cart
    - 最愛頁面: Favorites

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+import Header from "./components/header"
+import Home from "../src/pages/Home"
+import Member from "../src/pages/Member"
+import Cart from "../src/pages/Cart"
+import Favorites from "../src/pages/Favorites"
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Header />,
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/member" element={<Member />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/favorites" element={<Favorites />} />
+            </Routes>
+        </BrowserRouter>
+
+    );
 }
 
 export default App;
