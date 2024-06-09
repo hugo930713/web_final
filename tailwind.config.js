4746bd530f59908424eb3f86c86b10717910ed22
hugo930713 <hugo930713@gmail.com>
2024-06-09 12:29:21 +0800

    /** @type {import('tailwindcss').Config} */ export default {   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],   theme: {     extend: {},   },   plugins: [require("@tailwindcss/forms")], };

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  mode: 'jit',
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    container: {
-      center: true,
-    },
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
